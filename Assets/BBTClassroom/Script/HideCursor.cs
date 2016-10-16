4be7ee8b4798fc8e9ef33baf07467b3630d1a077
Hiroki Omae <pegion6@gmail.com>
2016-10-16 17:08:46 +0900

    Initial version

---
+++ Assets/BBTClassroom/Script/HideCursor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+[NodeInfo ( category = "Action/Cursor/",
+	icon = "CharacterController")]
+public class HideCursor : ActionNode {
+
+	public BoolVar hideCursor;
+
+	public override void Reset () {
+		hideCursor 	= new ConcreteBoolVar();
+	}
+
+	public override void Awake () {
+		Cursor.visible = !hideCursor.Value;
+	}
+
+	public override Status Update () {
+
+		Cursor.visible = !hideCursor.Value;
+
+		return Status.Success;
+	}
+}
---
+++ Assets/BBTClassroom/Script/QuaternionAngleAxis.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+[NodeInfo ( category = "Action/Math/",
+	icon = "CharacterController")]
+public class QuaternionAngleAxis : ActionNode {
+
+	public FloatVar angle;
+	public Vector3Var angleVector;
+	public Vector3Var setVector;
+	public FloatVar angleMultiplier;
+
+	public override void Reset () {
+		angle 		= new ConcreteFloatVar();
+		angleVector = new ConcreteVector3Var();
+		setVector 	= new ConcreteVector3Var();
+		angleMultiplier = new ConcreteFloatVar();
+	}
+
+	public override void Awake () {
+	}
+
+	public override Status Update () {
+
+		setVector.Value = Quaternion.AngleAxis (angle.Value * angleMultiplier.Value, angleVector.Value) * setVector.Value;
+
+		return Status.Success;
+	}
+}
