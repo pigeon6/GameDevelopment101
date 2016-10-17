1a69b0580eb5a0fb9a899c08736cb6fc9ba1c5ee
Hiroki Omae <pegion6@gmail.com>
2016-10-18 08:45:59 +0900

    Organize assets, add Lesson10/11

---
+++ Assets/BBTClassroom/Script/CountGameObjectsByTag.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourMachine;
+
+[NodeInfo ( category = "Action/GameObject/",
+	icon = "CharacterController")]
+public class CountGameObjectsByTag : ActionNode {
+
+	public StringVar tag;
+	public IntVar count;
+
+	public override void Reset () {
+		tag 		= new ConcreteStringVar();
+		count = new ConcreteIntVar();
+	}
+
+	public override void Awake () {
+	}
+
+	public override Status Update () {
+
+		if(tag.Value == null) {
+			return Status.Error;
+		}
+
+		GameObject[] objects = GameObject.FindGameObjectsWithTag(tag.Value);
+
+		count.Value = objects.Length;
+
+		return Status.Success;
+	}
+}
